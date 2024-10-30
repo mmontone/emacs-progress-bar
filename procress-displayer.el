aa5474b84fc599b9ba82eeb53d587b56a76b4d52
Mariano Montone <marianomontone@gmail.com>
2024-10-30 13:57:15 -0300

    procress-displayer

---
+++ procress-displayer.el
@@ -0,0 +1,31 @@
+;;; procress-displayer.el --- Progress displayer on the mode-line with animation  -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2024  Mariano Montone
+
+;; Author: Mariano Montone <marianomontone@gmail.com>
+;; Keywords: tools, convenience, extensions
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; Progress displayer on the mode-line with animation
+;; See: https://github.com/haji-ali/procress/blob/master/procress.el
+
+;;; Code:
+
+
+
+(provide 'procress-displayer)
+;;; procress-displayer.el ends here
