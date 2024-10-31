698a8eaad4552222637949bc1ed5574d7f0f05a0
Mariano Montone <marianomontone@gmail.com>
2024-10-31 14:34:22 -0300

    integrations

--- README.md
+++ README.md
@@ -6,22 +6,22 @@ A progress displayer for Emacs
 
 ## Install
 
-`progress-bar.el` has the progress bar implementation.
+`progress.el` has the progress bar implementation.
 
-Load `progress-bar-integrations.el` for integrating the progress bar in some of Emacs operations, like package management.
+Load `progress-integrations.el` for integrating the progress bar in some of Emacs operations, like package management.
 
 ## Usage
 
-The preferred method for using a progress-bar is via the utility functions:
-`dolist-with-progress-bar`, `dotimes-with-progress-bar` and `mapc-with-progress-bar`.
+The preferred method for using a progress is via the utility functions:
+`progress-dolist`, `dotimes-with-progress` and `mapc-with-progress`.
 
 Example:
 
 ```lisp
-(dolist-with-progress-bar (x (cl-loop for i from 1 to 10 collect i)
+(progress-dolist (x (cl-loop for i from 1 to 10 collect i)
                               :status-message (list "Started ..."
                                                     (lambda (pb)
-                                                      (format "Processing %s..." (progress-bar-data pb)))
+                                                      (format "Processing %s..." (progress-data pb)))
                                                     "Completed!"))
      (sit-for (seq-random-elt '(0.3 0.4 0.5))))
 ```
--- progress-integrations.el
+++ progress-integrations.el
@@ -1,11 +1,11 @@
-;;; progress-bar-integrations.el --- Integrations of progress-bar into Emacs.  -*- lexical-binding: t; -*-
+;;; progress-integrations.el --- Integrations of progress into Emacs.  -*- lexical-binding: t; -*-
 
 ;; Copyright (C) 2024  Free Software Foundation, Inc.
 
 ;; Author: Mariano Montone <marianomontone@gmail.com>
 ;; Keywords: tools, convenience, extensions
 ;; Version: 0.4
-;; Package-Requires: ((emacs "27.1") (progress-bar "0.5"))
+;; Package-Requires: ((emacs "27.1") (progress "0.5"))
 
 ;; This program is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
@@ -22,15 +22,15 @@
 
 ;;; Commentary:
 
-;; Integrates progress-bar into Emacs.
+;; Integrates progress into Emacs.
 ;;
 ;; As of this moment, this package overwrites some of the package.el operations
 ;; in order to display a progress bar for them; and makes Emacs progress-reporter
-;; work with progress-bars too.
+;; work with progresss too.
 
 ;;; Code:
 
-(require 'progress-bar)
+(require 'progress)
 (require 'package)
 
 ;; Packages
@@ -57,13 +57,13 @@ from ELPA by either using `\\[package-upgrade]' or
                          (format "%s packages to upgrade.  Do it?"
                                  (length upgradeable))))))
         (user-error "Upgrade aborted"))
-      (dolist-with-progress-bar (package upgradeable
-                                         :status-message
-                                         (list "Upgrading packages"
-                                               (lambda (pb)
-                                                 (format "Upgrading package: %s"
-                                                         (package-desc-name (progress-bar-data pb))))
-                                               (format "%d packages upgraded." (length upgradeable))))
+      (progress-dolist (package upgradeable
+                                :status-message
+                                (list "Upgrading packages"
+                                      (lambda (pb)
+                                        (format "Upgrading package: %s"
+                                                (package-desc-name (progress-data pb))))
+                                      (format "%d packages upgraded." (length upgradeable))))
           (package-upgrade package)))))
 
 (define-advice package-menu--perform-transaction (:override (install-list delete-list))
@@ -75,10 +75,10 @@ Return nil if there were no errors; non-nil otherwise."
                                      (length install-list)))
               (i 0)
               (package-menu--transaction-status))
-          (dolist-with-progress-bar (pkg install-list
-                                         :status-message (list (format "Installing %d packages" (length install-list))
-                                                               (lambda (pb) (format "Installing package: %s" (package-desc-name (progress-bar-data pb))))
-                                                               (format "%d packages installed." (length install-list))))
+          (progress-dolist (pkg install-list
+                                :status-message (list (format "Installing %d packages" (length install-list))
+                                                      (lambda (pb) (format "Installing package: %s" (package-desc-name (progress-data pb))))
+                                                      (format "%d packages installed." (length install-list))))
               (setq package-menu--transaction-status
                     (format status-format (cl-incf i)))
             (force-mode-line-update)
@@ -89,12 +89,12 @@ Return nil if there were no errors; non-nil otherwise."
     (let ((package-menu--transaction-status ":Deleting"))
       (force-mode-line-update)
       (redisplay 'force)
-      (dolist-with-progress-bar (elt (package--sort-by-dependence delete-list)
-                                     :status-message
-                                     (list (format "Deleting %d packages" (length delete-list))
-                                           (lambda (pb)
-                                             (format "Deleting package: %s" (package-desc-name (progress-bar-data pb))))
-                                           (format "%d packages deleted." (length delete-list))))
+      (progress-dolist (elt (package--sort-by-dependence delete-list)
+                            :status-message
+                            (list (format "Deleting %d packages" (length delete-list))
+                                  (lambda (pb)
+                                    (format "Deleting package: %s" (package-desc-name (progress-data pb))))
+                                  (format "%d packages deleted." (length delete-list))))
           (condition-case-unless-debug err
               (let ((inhibit-message (or inhibit-message package-menu-async)))
                 (package-delete elt nil 'nosave))
@@ -105,10 +105,10 @@ Return nil if there were no errors; non-nil otherwise."
     errors))
 
 (define-advice package--download-and-read-archives (:override (&optional async))
-  (dolist-with-progress-bar (archive package-archives
-                                     :status-message (list (format "Refreshing %d package archives" (length package-archives))
-                                                           (lambda (pb) (format "Reading archive: %s" (car (progress-bar-data pb))))
-                                                           "Package contents refreshed"))
+  (progress-dolist (archive package-archives
+                            :status-message (list (format "Refreshing %d package archives" (length package-archives))
+                                                  (lambda (pb) (format "Reading archive: %s" (car (progress-data pb))))
+                                                  "Package contents refreshed"))
       (condition-case-unless-debug nil
           (package--download-one-archive archive "archive-contents" async)
         (error (message "Failed to download `%s' archive."
@@ -116,44 +116,54 @@ Return nil if there were no errors; non-nil otherwise."
 
 ;; Progress reporter
 
-(defvar progress-reporter-progress-bars
+(defvar progress-reporter-progresses
   (make-hash-table :weakness 'key)
-  "A map of PROGRESS-REPORTER instances pointing to PROGRESS-BAR instances.")
+  "A map of PROGRESS-REPORTER instances pointing to PROGRESS instances.")
 
-(defun progress-reporter->progress-bar (reporter value)
-  "Convert progress REPORTER and current VALUE to a `progress-bar'.
-If a `progress-bar' has already been created, then update its `current-step' and return it."
+(defun progress-reporter->progress (reporter value)
+  "Convert progress REPORTER and current VALUE to a `progress'.
+If a `progress' has already been created, then update its `current-step' and return it."
   (let* ((parameters   (cdr reporter))
          ;;(update-time  (aref parameters 0))
          (min-value    (aref parameters 1))
          (max-value    (aref parameters 2))
          (text         (aref parameters 3)))
-    (let ((progress-bar (or (gethash reporter progress-reporter-progress-bars)
-                            (puthash reporter
-                                     (make-progress-bar
-                                      :total-steps (- max-value min-value)
-                                      :status-message text)
-                                     progress-reporter-progress-bars))))
-      (with-slots (current-step) progress-bar
+    (let ((progress (or (gethash reporter progress-reporter-progresses)
+                        (let ((progress (make-progress
+                                         :total-steps (- max-value min-value)
+                                         :status-message text)))
+                          (puthash reporter progress
+                                   progress-reporter-progresses)
+                          (progress-notify 'started progress)
+                          progress))))
+      (with-slots (current-step) progress
         (setf current-step (- value min-value)))
-      progress-bar)))
+      progress)))
 
-(defcustom progress-bar-replace-progress-reporter t
+(defcustom progress-replace-progress-reporter t
   "When enabled, use a progress bar instead of default Emacs progress reporter."
   :type 'boolean
-  :group 'progress-bar)
+  :group 'progress)
 
-(defun progress-bar-around-reporter (orig reporter value &optional suffix)
-  (if (not progress-bar-replace-progress-reporter)
+(defun progress-around-reporter (orig reporter value &optional suffix)
+  (if (not progress-replace-progress-reporter)
       (funcall orig reporter value suffix)
     (let* ((parameters   (cdr reporter))
            (min-value    (aref parameters 1))
            (max-value    (aref parameters 2)))
       (if (and (not min-value) (not max-value))
           (funcall orig reporter value suffix)
-        (progress-bar--display (progress-reporter->progress-bar reporter value))))))
+        ;; TODO: fixme
+        ;;(progress-update (progress-reporter->progress reporter)
+        ;;                 'current-step value)
+        (progress-incf (progress-reporter->progress reporter value))
+        ))))
 
-(advice-add 'progress-reporter-do-update :around 'progress-bar-around-reporter)
+(defun progress-reporter-done-override (reporter)
+  (progress-notify 'completed (progress-reporter->progress reporter 0)))
+
+(advice-add 'progress-reporter-do-update :around 'progress-around-reporter)
+(advice-add 'progress-reporter-done :override 'progress-reporter-done-override)
 
 (when nil
   (let ((progress-reporter
@@ -164,6 +174,6 @@ If a `progress-bar' has already been created, then update its `current-step' and
       (progress-reporter-update progress-reporter k))
     (progress-reporter-done progress-reporter)))
 
-(provide 'progress-bar-integrations)
+(provide 'progress-integrations)
 
-;;; progress-bar-integrations.el ends here
+;;; progress-integrations.el ends here
