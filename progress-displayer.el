249bb79f52964d44e0ad80e6251e211eea6e889e
Mariano Montone <marianomontone@gmail.com>
2024-11-12 10:14:37 -0300

    package versions

--- progress-displayer.el
+++ progress-displayer.el
@@ -4,6 +4,7 @@
 
 ;; Author: Mariano Montone <marianomontone@gmail.com>
 ;; Keywords: tools, convenience, extensions
+;; Version: 0.1
 
 ;; This program is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
